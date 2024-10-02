9d98ea8018711c866df5adec198e05e1f2e2990a
Daria <Dasha-esha@mail.ru>
2024-10-02 15:36:43 +0500

    update

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,8 @@
 public class Main {
+    public static void main(String[] args) {
+        int price = 14_500;
+        int bonus = 20;
+        int x = price / bonus;
+        System.out.println(x);
+    }
 }
